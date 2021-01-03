198fdf7e5d8e76c81a75d69d2e4a0621b9ab142d
LundrimRexhepi <lundrim4@hotmail.com>
2021-01-03 19:29:10 +0100

    test

--- eBibloteka/Controllers/HomeController.cs
+++ eBibloteka/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace eBibloteka.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-            var test = "";
+            var test = "never used";
             return View();
         }
     }
