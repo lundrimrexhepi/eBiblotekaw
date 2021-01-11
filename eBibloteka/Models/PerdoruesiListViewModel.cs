a94b9f59e86266cadafc5e4127fcfa8fc7f15c4f
LundrimRexhepi <lundrim4@hotmail.com>
2021-01-12 00:26:55 +0100

    m\s

--- eBibloteka/Models/PerdoruesiListViewModel.cs
+++ eBibloteka/Models/PerdoruesiListViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -20,5 +21,42 @@ namespace eBibloteka.Models
         public int GrupiID { get; set; }
         public System.DateTime DataInsertimit { get; set; }
         public string Perdoruesi { get; set; }
+    }
+    public class PerdoruesiModel
+    {
+        public int perdoruesiID { get; set; }
+
+        [Required(ErrorMessage = "Shkruani perdoruesin!")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Shkruani emrin!")]
+        public string Emri { get; set; }
+
+        [Required(ErrorMessage = "Shkruani mbiemrin!")]
+        public string Mbiemri { get; set; }
+
+        [Required(ErrorMessage = "Shkruani numrin personal!")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Lejohen vetem numrat!")]
+        public string NumriPersonal { get; set; }
+
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Lejohen vetem numrat!")]
+        public string Telefoni { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid E-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Shkruaj fjalekalimin!")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$", ErrorMessage = "Fjalekalimi duhet te kete shkronje te madhe, shkronje te vogel, numer dhe 8 e me shume karaktere!")]
+        public string Fjalekalimi { get; set; }
+
+        [Required(ErrorMessage = "Zgjedh grupin!")]
+        public long[] grupiID { get; set; }
+
+        [Required(ErrorMessage = "Zgjedh komunat!")]
+        public long[] komunaID { get; set; }
+
+        public bool allKomunat { get; set; }
+
+        public bool Aktive { get; set; }
     }
 }
